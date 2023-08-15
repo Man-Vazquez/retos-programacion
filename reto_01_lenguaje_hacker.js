9f601c7df272e78401c8b86fd37d72341cd54b71
jmvazquezc <jose.mtto94@gmail.com>
2023-08-14 23:09:57 -0600

    Se agrega segundo reto #01 lenguaje hacker con mi solución

--- reto_01_lenguaje_hacker.js
+++ reto_01_lenguaje_hacker.js
@@ -10,3 +10,45 @@
  *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
  */
 
+function encryption(word){
+    const alphabet = [
+        { letter: 'a', leet: '4' },
+        { letter: 'b', leet: 'I3' },
+        { letter: 'c', leet: '[' },
+        { letter: 'd', leet: ')' },
+        { letter: 'e', leet: '3' },
+        { letter: 'f', leet: '|=' },
+        { letter: 'g', leet: '&' },
+        { letter: 'h', leet: '#' },
+        { letter: 'i', leet: '1' },
+        { letter: 'j', leet: ',_|' },
+        { letter: 'k', leet: '>|' },
+        { letter: 'l', leet: '£' },
+        { letter: 'm', leet: '/V\\' },
+        { letter: 'n', leet: '^/' },
+        { letter: 'o', leet: '0' },
+        { letter: 'p', leet: '|*' },
+        { letter: 'q', leet: '(_,)' },
+        { letter: 'r', leet: 'I2' },
+        { letter: 's', leet: '5' },
+        { letter: 't', leet: '7' },
+        { letter: 'u', leet: '(_)' },
+        { letter: 'v', leet: '|/' },
+        { letter: 'w', leet: 'Ш' },
+        { letter: 'x', leet: '><' },
+        { letter: 'y', leet: 'j' },
+        { letter: 'z', leet: '7_' },
+        { letter: ' ', leet: ' ' }
+    ];
+
+    let textEncryption = '';
+
+    for(let i = 0; i < word.length; i++){
+        let findLetter = word[i];
+        let textLetter = alphabet.find((text) => text.letter === findLetter);
+        textEncryption += textLetter.leet;
+    }
+    console.log(textEncryption)
+}
+
+encryption('hola mundo');
